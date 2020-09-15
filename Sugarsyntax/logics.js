ea7a623060db919d63ab5642b967bb898535ddaa
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-09-15 15:03:48 +0300

    add project

---
+++ Sugarsyntax/logics.js
@@ -0,0 +1,35 @@
+var st= "Mamas {number} Empire!";
+var dict = {};
+dict["{number}"]="6";
+
+function StringFormatter(string, dictionary){
+    var dictkeys= Object.keys(dictionary);
+    var count=0;
+    for (var i=0;i<dictkeys.length;i++)
+    {
+        var newstr="";
+        console.log(dictkeys[i]);
+        if(string.indexOf(dictkeys[i])!=-1)
+        {
+            for (var j=0;j<string.indexOf(dictkeys[i]);j++)
+            {
+                newstr+=string[j];
+            }
+            newstr+=dictionary[dictkeys[i]];
+            for(var p=string.indexOf(dictkeys[i])+dictkeys[i].length;p<string.length;p++)
+            {
+                newstr+=string[p];
+            }
+        }
+        else{
+            count++;
+        }
+    }
+    if(count>0)
+        return "Error ! Not Found";
+    else
+        return newstr;
+}
+
+
+alert(StringFormatter(st,dict));
